e33a4357bec202ddf4926ff23da4a88230fbabbb
myselfayansalim <ayansalim1605+1@gmail.com>
2026-06-05 18:09:32 +0530

    remove the insta link

--- config/socials.ts
+++ config/socials.ts
@@ -20,13 +20,7 @@ export const SocialLinks: SocialInterface[] = [
     icon: Icons.linkedin,
     link: "https://www.linkedin.com/in/ayan-shaikh-a222b7261",
   },
-  {
-    name: "Instagram",
-    username: "@ayan_salim_1605",
-    icon: Icons.instagram,
-    link: "https://www.instagram.com/ayan_salim_1605/",
-  },
-  {
+{
     name: "Gmail",
     username: "ayansalim1605",
     icon: Icons.gmail,
